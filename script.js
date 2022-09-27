864c85c728b598c5fb0bff27494fc3391f7e32e2
Meron Yohannes <myohannes268@gmail.com>
2022-09-28 02:32:21 +0300

    form prepared

---
+++ script.js
@@ -0,0 +1,33 @@
+// Array of Objects
+const quiz = [
+    {
+        q:'you are 3rd place right now in a race. What place are you in when you pass the person in 2nd place?',
+        option:['1st','2nd','3rd','none'],
+        answer:1
+    },
+    {
+        q:'How many months have 28 days ?',
+        option:['2','1','all of them','Depends if there is a leap year or not'],
+        answer:2
+    },
+    {
+        q:'What is the name of our group',
+        option:['Enterprenuers','Brithtness', 'html', 'javaScript'],
+        answer:0
+    },
+    {
+        q:'The date of today is 18/5/22',
+        option:['true','false'],
+        answer:1
+    },
+    {
+        q:'What is always coming but never arrives ?',
+        option:['car','lion','tomorrow','none'],
+        answer:2
+    },
+    {
+        q:'What is 22 + 6 ?',
+        option:['99','56','16','28'],
+        answer:3
+    }
+]
