9002ab0b2e744d739b55801479818d67ed7b443a
Meron Yohannes <myohannes268@gmail.com>
2022-10-31 01:14:34 +0300

    adu questions changed

--- script.js
+++ script.js
@@ -1,33 +1,33 @@
 // Array of Objects
 const quiz = [
     {
-        q:'where was Jesus born?',
-        option:['Addis-Abeba','Bethlehem','Jerusalem','none'],
-        answer:1
+        q:'exclude one which is not member of ur family?',
+        option:['yordanos','senait','helen','none'],
+        answer:3
     },
     {
-        q:'How many fish did Jesus need to feed the 5000 people?',
-        option:['2','1','5000','no answer'],
+        q:'if ur tears roll down first from ur right eye. it means..',
+        option:['tears of happiness','tears of suffering','tears of worrying','none'],
         answer:0
     },
     {
-        q:'Adiam s favoriate color?',
-        option:['blue','black', 'silver', 'white'],
-        answer:1
+        q:'adiam s birthday?',
+        option:['4 february','14 february', "saint valentine's day", 'all except 4 february'],
+        answer:3
     },
     {
-        q:'Adiam loves her family so much!',
+        q:'u r blessed!',
         option:['true','false'],
         answer:0
     },
     {
-        q:'which number is most loved by Adu?',
-        option:['4','1','3','0'],
+        q:'how many letter does adiam has?',
+        option:['5','1','3','4'],
         answer:0
     },
     {
-        q:'Who approached Jesus in the middle of the night?',
-        option:['Alazar','Adiam','Meron','Nicodemus'],
-        answer:3
+        q:'what is the meaning of fish in early christians?',
+        option:["Jesus Christ God's Son Savior",'food','catholic','none'],
+        answer:0
     }
 ]
